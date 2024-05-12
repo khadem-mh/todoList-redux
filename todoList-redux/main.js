ad991d7b3355031494e9253adfd2dfad893ef5eb
Mohammad Hossein Khadem al - Mahdi <101788797+khadem-mh@users.noreply.github.com>
2024-05-12 09:26:26 +0330

    styling

--- todoList-redux/main.js
+++ todoList-redux/main.js
@@ -1,1 +1,8 @@
 import { createStore } from 'redux'
+
+const $ = document
+const inputElemTodo = $.getElementById("inputElemTodo")
+const divElemMain = $.getElementById('mainTodos')
+const addTodoBtn = $.getElementById("addTodoBtn")
+const clearTodoBtn = $.getElementById("clearTodoBtn")
+const alert = $.getElementById('alert')
