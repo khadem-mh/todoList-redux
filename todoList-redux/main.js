21504b3a8e2a7a164b95bdfa520c0f811679dd44
Mohammad Hossein Khadem al - Mahdi <101788797+khadem-mh@users.noreply.github.com>
2024-05-12 16:39:53 +0330

    fix bug

--- todoList-redux/main.js
+++ todoList-redux/main.js
@@ -9,26 +9,34 @@ const inputElemTodo = $.getElementById("inputElemTodo")
 const divElemMain = $.getElementById('mainTodos')
 const addTodoBtn = $.getElementById("addTodoBtn")
 const clearTodoBtn = $.getElementById("clearTodoBtn")
-const containerMainTodos = $.getElementById("clearTodoBtn")
+const containerMainTodos = $.getElementById("mainTodos")
 const alert = $.getElementById('alert')
 
 const store = createStore(reducer)
 
 const addTodoHandler = () => {
-    store.dispatch(addTodoAction(inputElemTodo.target.value))
-    
-    containerMainTodos.insertAdjacentHTML('beforeend', `
+    let inpElemVal = inputElemTodo.target.value
+    if (inpElemVal.length) {
+        store.dispatch(addTodoAction(inpElemVal))
+
+        containerMainTodos.insertAdjacentHTML('beforeend', `
         <section class='card card-body bg-transparent border-success mt-2'>
           <div class='d-flex flex-row-reverse align-items-center justify-content-between'>
             <div class='btn-group'>
               <i class="btn btn-outline-success">Complete</i>
               <i class="btn btn-outline-danger">Delete</i>
             </div>
-            <p class='card-title text-light h5 mb-0 bg-dark py-2 px-3 me-3 rounded overflow-auto fst-italic'>${inputElemTodo.target.value}
+            <p class='card-title text-light h5 mb-0 bg-dark py-2 px-3 me-3 rounded overflow-auto fst-italic'>${inpElemVal}
             </p>
           </div>
         </section>
     `)
+        inpElemVal.classList.remove('lineOver')
+        alert.innerHTML = 'Success'
+    } else {
+        inpElemVal.classList.add('lineOver')
+        alert.innerHTML = 'Error : Input Empty'
+    }
 }
 
 addTodoBtn.addEventListener('click', addTodoHandler)
