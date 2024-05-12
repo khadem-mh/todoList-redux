3323a03758beef5c2349cbeeef1a3f6483bd8d23
Mohammad Hossein Khadem al - Mahdi <101788797+khadem-mh@users.noreply.github.com>
2024-05-12 11:35:58 +0330

    add action creators

--- todoList-redux/src/Redux/todoList.js
+++ todoList-redux/src/Redux/todoList.js
@@ -7,3 +7,23 @@ export const completeTodo = 'COMPLETE_TODO'
 // Reducer
 
 // Action Creators
+const addTodoAction = () => {
+    return {
+        type: addTodo,
+    }
+}
+const clearAllTodosAction = () => {
+    return {
+        type: clearAllTodos,
+    }
+}
+const removeTodoAction = () => {
+    return {
+        type: removeTodo,
+    }
+}
+const completeTodoAction = () => {
+    return {
+        type: completeTodo,
+    }
+}
