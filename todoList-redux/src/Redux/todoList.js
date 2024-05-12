6062e320764fb91445202775159e7c294d6abb32
Mohammad Hossein Khadem al - Mahdi <101788797+khadem-mh@users.noreply.github.com>
2024-05-12 11:34:33 +0330

    add file todoList.js

---
+++ todoList-redux/src/Redux/todoList.js
@@ -0,0 +1,9 @@
+// Actions
+export const addTodo = 'ADD_TODO'
+export const clearAllTodos = 'CLEAR_ALL_TODOS'
+export const removeTodo = 'REMOVE_TODO'
+export const completeTodo = 'COMPLETE_TODO'
+
+// Reducer
+
+// Action Creators
